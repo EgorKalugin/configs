a45b9cc55b4860bd647a36420c5656f0fdd77500
Egor Kalugin <egorkavoron@gmail.com>
2025-06-16 16:11:57 +0300

    update rust lsp conifg

--- .config/nvim/lua/configs/rustacean.lua
+++ .config/nvim/lua/configs/rustacean.lua
@@ -1,32 +1,52 @@
-local nvlsp = require "nvchad.configs.lspconfig"
-local mason_registry = require "mason-registry"
-local codelldb = mason_registry.get_package "codelldb"
-local extension_path = codelldb:get_install_path() .. "/extension/"
-local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
--- If you are on Linux, replace the line above with the line below:
--- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-local cfg = require "rustaceanvim.config"
+vim.g.rustaceanvim = function()
+  local nvlsp = require "nvchad.configs.lspconfig"
+  local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
+  local codelldb_path = extension_path .. "adapter/codelldb"
+  local liblldb_path = extension_path .. "lldb/lib/liblldb"
 
-vim.g.rustaceanvim = {
-  dap = {
-    adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-  },
-  server = {
-    on_attach = nvlsp.on_attach,
-    default_settings = {
-      ["rust-analyzer"] = {
-        assist = {
-          importMergeBehavior = "last",
-          importPrefix = "by_self",
-        },
-        cargo = {
-          allFeatures = true,
-        },
-        procMacro = {
-          enable = true,
+  local this_os = vim.uv.os_uname().sysname
+  if this_os:find "Windows" then
+    codelldb_path = extension_path .. "adapter\\codelldb.exe"
+    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+  else
+    -- The liblldb extension is .so for Linux and .dylib for MacOS
+    liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+  end
+
+  local cfg = require "rustaceanvim.config"
+
+  return {
+    dap = {
+      adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+    server = {
+      on_attach = nvlsp.on_attach,
+      default_settings = {
+        ["rust-analyzer"] = {
+          assist = {
+            importPrefix = "by_self",
+          },
+          cargo = {
+            loadOutDirsFromCheck = true,
+            runBuildScripts = true,
+            buildScripts = true,
+            allFeatures = true,
+          },
+          check = {
+            command = "clippy",
+            allFeatures = true,
+          },
+          procMacro = {
+            enable = true,
+          },
+          inlayHints = {
+            lifetimeElisionHints = {
+              enable = true,
+              useParameterNames = true,
+            },
+          },
         },
       },
     },
-  },
-}
+  }
+end
