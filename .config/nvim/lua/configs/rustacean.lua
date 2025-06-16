659f51d8ac70d62ea74192eee18509fbed9e506b
Egor Kalugin <egorkavoron@gmail.com>
2025-06-16 16:24:10 +0300

    improve config (?)

--- .config/nvim/lua/configs/rustacean.lua
+++ .config/nvim/lua/configs/rustacean.lua
@@ -21,6 +21,8 @@ vim.env.RUST_ANALYZER_MEMORY_LIMIT = "8192" -- 8GB limit
 local rust_analyzer_config = {
   assist = {
     importPrefix = "by_self",
+    importGranularity = "module",
+    importEnforceGranularity = true,
   },
   cargo = {
     loadOutDirsFromCheck = true,
@@ -28,33 +30,122 @@ local rust_analyzer_config = {
     buildScripts = true,
     allFeatures = true,
     targetDir = "target/rust-analyzer",
+    features = "all",
+    noDefaultFeatures = false,
+    noSysroot = false,
   },
   check = {
     command = "clippy",
     allFeatures = true,
     extraArgs = { "--target-dir", "target/rust-analyzer" },
+    args = { "--target-dir", "target/rust-analyzer" },
   },
   procMacro = {
     enable = true,
   },
   inlayHints = {
     enable = true,
+    typeHints = {
+      enable = true,
+      hideClosureInitialization = false,
+      hideNamedConstructor = false,
+    },
+    chainingHints = {
+      enable = true,
+      maxLength = 25,
+    },
+    parameterHints = {
+      enable = true,
+      showNamedParameters = true,
+    },
     lifetimeElisionHints = {
       enable = true,
       useParameterNames = true,
     },
+    reborrowHints = {
+      enable = true,
+      showReborrows = true,
+    },
+    expressionAdjustmentHints = {
+      enable = true,
+      mode = "prefix",
+    },
   },
   files = {
-    excludeDirs = { ".git", "target" },
+    excludeDirs = { ".git", "target", "node_modules", "dist", "build", ".venv" },
     watcher = "client",
     watcherExclude = {
       "**/target/**",
       "**/Cargo.lock",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/.venv/**",
     },
   },
   diagnostics = {
     enable = true,
     experimental = {
+      enable = true,
+    },
+    disabled = {},
+    warningsAsHint = {},
+    warningsAsInfo = {},
+  },
+  lens = {
+    enable = true,
+    references = true,
+    implementations = true,
+    run = true,
+    debug = true,
+    gotoTypeDef = true,
+    methodReferences = true,
+    referencesADT = true,
+    enumVariantReferences = true,
+    location = "above_whole_item",
+  },
+  hover = {
+    actions = {
+      enable = true,
+      implementations = true,
+      references = true,
+      run = true,
+      debug = true,
+      gotoTypeDef = true,
+    },
+    links = {
+      enable = true,
+    },
+    documentation = {
+      enable = true,
+    },
+  },
+  completion = {
+    autoimport = {
+      enable = true,
+    },
+    postfix = {
+      enable = true,
+    },
+    fullFunctionSignatures = true,
+    callable = {
+      snippets = "fill_arguments",
+    },
+  },
+  semanticHighlighting = {
+    enable = true,
+    operator = {
+      enable = true,
+    },
+    punctuation = {
+      enable = true,
+    },
+    strings = {
+      enable = true,
+    },
+  },
+  typing = {
+    autoClosingAngleBrackets = {
       enable = true,
     },
   },
