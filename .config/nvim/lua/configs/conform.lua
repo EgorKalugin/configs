0dcbd8059151e5f934e2721ed503709420493922
Egor Kalugin <egorkavoron@gmail.com>
2025-09-09 11:29:19 +0300

    add js/ts formatters

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,12 +1,16 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
     python = { "ruff_format", "isort", "ruff" },
     -- Prefer prettier for JSON; jq second (strict JSON only)
     json = { "prettier", "jq" },
     jsonc = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" }, -- JSX
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" }, -- TSX
   },
 
   format_on_save = nil,
